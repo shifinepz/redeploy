dbbdb28dd32d78f37d91c39bd788a3d2be7ee5ae
shifin rafeeque <shifin.epz@gmail.com>
2018-11-30 16:54:31 +0530

    Update redeploy.sh

--- redeploy.sh
+++ redeploy.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/bash
 
-#sudo yum install tomcat -y
 sudo sed -i 's/port="8080"/port="8080"/' /etc/tomcat/server.xml
-#sudo yum install tomcat-webapps tomcat-admin-webapps -y
-#sudo yum install tomcat-docs-webapp tomcat-javadoc -y
 sudo systemctl restart tomcat
 sudo systemctl status tomcat.service
 cp /opt/Devops/java/cacheImplement-2.4-SNAPSHOT.war /var/lib/tomcat/webapps/
